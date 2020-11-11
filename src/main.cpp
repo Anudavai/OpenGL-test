709aa706b56e0d673bec8f573744d287f9dbf666
Anudavai <poit25@ukr.net>
2020-11-11 03:16:13 +0200

    Added GLAD library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -6,7 +8,14 @@ int main(void)
 
 	/* Initialize the library */
 	if (!glfwInit())
+	{
+		std::cout << "glfwInit failed!" << std::endl;
 		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,11 +28,21 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGL())
+	{
+		std::cout << "Can't load GLAD!" << std::endl;
+		return -1;
+	}
+
+	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+	glClearColor(0, 1, 0, 1);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
-		//glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
